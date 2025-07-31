b53f5874671fa7195f9de756dbc3638534d656d7
smarch_vcare <smarch@visualcare.com.au>
2025-07-31 10:10:47 +0930

    Incorrect name for vite.config base correction

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Replace 'portfolio' with your repository name
+  base: '/Portfolio/', // Replace 'portfolio' with your repository name
   build: {
     assetsDir: 'assets',
     rollupOptions: {
